7c0f3052d2d32cf1e01576d3f374880cca4e5e65
Ach B <archibajl@gmail.com>
2025-11-15 00:49:19 -0700

    fixed migrations broken from main

--- active_interview_backend/active_interview_app/migrations/0009_interviewtemplate.py
+++ active_interview_backend/active_interview_app/migrations/0009_interviewtemplate.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('active_interview_app', '0007_alter_chat_type_userprofile_rolechangerequest'),
+        ('active_interview_app', '0008_tag_questionbank_question_interviewtemplate'),
     ]
 
     operations = [
